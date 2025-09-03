4a3f10a1291cdae345db0cb4fa63c5d8f1dc7230
Umoh Bassey-Duke <umohb@aku.africa>
2025-09-03 17:33:08 +0200

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $africa.akupay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
